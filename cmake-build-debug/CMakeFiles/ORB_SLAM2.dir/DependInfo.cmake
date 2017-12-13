7ed5d8be6dd1811ca6c603a0280f5a6b5cede67c
wmh123456789 <wmh123456789@qq.com>
2017-12-13 17:36:44 +0800

    build

---
+++ cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/Converter.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Converter.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/Frame.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Frame.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/FrameDrawer.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/FrameDrawer.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/Initializer.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Initializer.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/KeyFrame.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/KeyFrame.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/KeyFrameDatabase.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/KeyFrameDatabase.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/LocalMapping.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/LocalMapping.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/LoopClosing.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/LoopClosing.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/Map.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Map.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/MapDrawer.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/MapDrawer.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/MapPoint.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/MapPoint.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/ORBextractor.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/ORBextractor.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/ORBmatcher.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/ORBmatcher.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/Optimizer.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Optimizer.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/PnPsolver.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/PnPsolver.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/Sim3Solver.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Sim3Solver.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/System.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/System.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/Tracking.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Tracking.cc.o"
+  "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/src/Viewer.cc" "/home/wmh/work/ORB_SLAM2_wmh-ubuntu/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Viewer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/include/eigen3q"
+  "/usr/local/lib/cmake/Pangolin/../../../include"
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/mono_wmh.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mono_wmh.dir/Examples/Monocular/mono_wmh.cc.o"
+  "../Examples/Monocular/mono_wmh.pdb"
+  "../Examples/Monocular/mono_wmh"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mono_wmh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

aaf56eb2964dd2663e7ac8266a49fd744075ab8d
wmh123456789 <wmh123456789@qq.com>
2018-01-06 12:25:38 +0800

    Try kalman filter

--- src/System.cc
+++ src/System.cc
@@ -57,6 +57,10 @@ System::System(const string &strVocFile, const string &strSettingsFile, const eS
        exit(-1);
     }
 
+    // For debug    by wmh
+    TryKalmanFilter("aa.a");
+    // -- debug
+
 
     //Load ORB Vocabulary
     cout << endl << "Loading ORB Vocabulary. This could take a while..." << endl;
@@ -624,6 +628,20 @@ void System::SaveCurrentFrameInfo(const string &filename)
 void System::TryKalmanFilter(const string &filename)
 {
 
+    float X[10][3] =
+            {
+                    10,    50,     15.6,
+                    12,    49,     16,
+                    11,    52,     15.8,
+                    13,    52.2,   15.8,
+                    12.9,  50,     17,
+                    14,    48,     16.6,
+                    13.7,  49,     16.5,
+                    13.6,  47.8,   16.4,
+                    12.3,  46,     15.9,
+                    13.1,  45,     16.2
+            };
+
     float A[10][3] =
             {
                     10,    50,     15.6,
@@ -638,10 +656,14 @@ void System::TryKalmanFilter(const string &filename)
                     13.1,  45,     16.2
             };
 
+
+
     const int stateNum=3;
     const int measureNum=3;
     const float dt = 0.2;
+
     cv::KalmanFilter KF(stateNum, measureNum, 0);
+
     KF.transitionMatrix = (cv::Mat_<float>(3, 3) <<1,0,0,0,1,0,0,0,1);  //转移矩阵A
     setIdentity(KF.measurementMatrix);                                             //测量矩阵H
     setIdentity(KF.processNoiseCov, cv::Scalar::all(1e-5));                            //系统噪声方差矩阵Q

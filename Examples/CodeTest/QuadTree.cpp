c0772a5ff2942f7e425051c460b518ab291593e8
wmh123456789 <wmh123456789@qq.com>
2018-01-13 16:47:38 +0800

    Update QuadTree class. More functions are added.

--- Examples/CodeTest/QuadTree.cpp
+++ Examples/CodeTest/QuadTree.cpp
@@ -8,60 +8,165 @@
 
 namespace WMH
 {
+    QTNode::QTNode(int depth, float size, Point3f center, QTNode* parent)
+    {
+        this->mDepth = depth;
+        this->mSize = size;
+        this->mCenter = center;
+        this->mParent= parent;
+//        mChildren = NULL;
+//        mChildren[DL] = NULL;
+//        mChildren[DR] = NULL;
+//        mChildren[UR] = NULL;
+//        mChildren[UL] = NULL;
+
+        mMaxX = center.x + size/2;
+        mMinX = center.x - size/2;
+        mMaxY = center.y ;         // Don't use Y in cozmo
+        mMinY = center.y ;         // Don't use Y in cozmo
+        mMaxZ = center.z + size/2;
+        mMinZ = center.z - size/2;
+
+    }
+    void QTNode::setNodeId(int id){
+        mNodeId = id;
+    }
+
+    int QTNode::getNodeId(){
+        return mNodeId;
+    }
 
 
-QTNode::QTNode(int depth, float size, Point3f* center, QTNode* parent, QTContent* content)
-{
-    this->mDepth = depth;
-    this->mSize = size;
-    this->mCenter = center;
-    this->mParent= parent;
-    this->mContent = content;
+    bool QTNode::isPointIn(Point3f Pt){
+        return 0;
+    }
 
-    mChildren[DL] = NULL;
-    mChildren[DR] = NULL;
-    mChildren[UR] = NULL;
-    mChildren[UL] = NULL;
+    void QTNode::QueryNeighborNode(NeighborOrientation Orientation, QTNode* Node){
+        return;
+    }
 
-    mMaxX = center->x + size/2;
-    mMinX = center->x - size/2;
-    mMaxY = center->y ;         // Don't use Y in cozmo
-    mMinY = center->y ;         // Don't use Y in cozmo
-    mMaxZ = center->z + size/2;
-    mMinZ = center->z - size/2;
-
-
-
-
-    return ;
-
-
-}
-
-bool QTNode::isPointIn(Point3f* Pt){
-    return 0;
-}
-
-vector<QTNode*> QTNode::QueryNeighborNode(NeighborOrientation Orientation){
-    return NULL;
-}
-
-int QTNode::InitChildren(QTContent* content){
-    return 0;
-}
-
-
-QuadTree::QuadTree()
-{
-    mpWord = "Hello WMH.";
-}
+    int QTNode::InitChildren(QTContent* content){
+        Point3f Pt;
+        Point3f center;
+        QTContent ContDL,ContDR,ContUL,ContUR;
 
 
 
-void QuadTree::SayHello(const string &something)
-{
-    cout << mpWord << something << endl;
-}
+
+        // Loop over all points
+        for(int i =0; i < content->MapPts.size(); i++)
+        {
+            Pt = *content->MapPts[i];
+            // Allocate points into Quad-Area
+            if (isPointIn(Pt)){
+                if (Pt.x < mCenter.x){         // Left
+                    if(Pt.z < mCenter.z)       // Down
+                        ContDL.MapPts.insert(ContDL.MapPts.begin(),&Pt);
+                    else                       // Up
+                        ContUL.MapPts.insert(ContUL.MapPts.begin(),&Pt);
+                }
+                else{                          // Right
+                    if(Pt.z > mCenter.z)       // Down
+                        ContDR.MapPts.insert(ContDR.MapPts.begin(),&Pt);
+                    else                       // Up
+                        ContUR.MapPts.insert(ContUR.MapPts.begin(),&Pt);
+                }
+            }
+        }
+        ContDL.MapPtN = ContDL.MapPts.size();
+        ContDR.MapPtN = ContDR.MapPts.size();
+        ContUL.MapPtN = ContUL.MapPts.size();
+        ContUR.MapPtN = ContUR.MapPts.size();
+
+        // Calculate the sub-center and set content for children
+        // TODO: Check depth before new children
+        mChildren.insert(mChildren.begin(),4,QTNode(mDepth-1,mSize/2,mCenter,this));
+
+        center.y = mCenter.y;  // y is not changed in Cozmo.
+
+        center.x = mCenter.x - mSize/4;
+        center.z = mCenter.z - mSize/4;
+        mChildren[DL].setCenter(center); //TODO
+        mChildren[DL].setContent(ContDL);
+//
+//        center.x = mCenter.x + mSize/4;
+//        center.z = mCenter.z - mSize/4;
+//        mChildren[DR] = QTNode(mDepth-1,mSize/2,center,this);
+//        mChildren[DR].setContent(ContDR);
+//
+//        center.x = mCenter.x - mSize/4;
+//        center.z = mCenter.z + mSize/4;
+//        mChildren[UL] = QTNode(mDepth-1,mSize/2,center,this);
+//        mChildren[UL].setContent(ContUL);
+//
+//        center.x = mCenter.x + mSize/4;
+//        center.z = mCenter.z + mSize/4;
+//        mChildren[UR] = QTNode(mDepth-1,mSize/2,center,this);
+//        mChildren[UR].setContent(ContUR);
+
+        return 0;
+    }
+
+    void QTNode::PrintNodeInfo() {
+        cout << "NodeID:" << mNodeId << endl;
+        cout << "Center X: "<< mCenter.x << endl;
+        cout << "Center Z: "<< mCenter.z << endl;
+        cout << "Depth:" << mDepth << "; Size:"<< mSize << endl;
+        cout << "X: From " << mMinX << " to " << mMaxX << endl;
+        cout << "Z: From " << mMinZ << " to " << mMaxZ << endl;
+        cout << "NodeID:" << mNodeId << endl;
+
+    }
+
+    void QTNode::setContent(const QTContent &mContent) {
+        QTNode::mContent = mContent;
+    }
+
+    const QTContent &QTNode::getContent() const {
+        return mContent;
+    }
+
+    void QTNode::setCenter(const Point3f &mCenter) {
+        QTNode::mCenter = mCenter;
+    }
+
+    const Point3f &QTNode::getCenter() const {
+        return mCenter;
+    }
+
+
+    QuadTree::QuadTree()
+    {
+        mpWord = "Hello WMH.";
+    }
+
+    QuadTree::QuadTree(int MaxDepth,vector<Point3f*> MapPoints) {
+
+        Point3f center;
+        center.x = 0.0;
+        center.y = 0.0;
+        center.z = 0.0;
+
+        QTContent content = FillContentWithMapPoints(MapPoints);
+        QTNode node = QTNode(MaxDepth, 32.0, center, NULL);
+        RootNode = &node;
+        RootNode->setNodeId(-1);
+
+
+    }
+
+    QTContent QuadTree::FillContentWithMapPoints(vector<Point3f*> MapPoints) {
+
+        QTContent content;
+        content.MapPts = MapPoints;
+        content.MapPtN = MapPoints.size();
+
+        return content;
+    }
+    void QuadTree::SayHello(const string &something)
+    {
+        cout << mpWord << something << endl;
+    }
 
 }
 
--- Examples/CodeTest/QuadTree.h
+++ Examples/CodeTest/QuadTree.h
@@ -11,16 +11,18 @@
 #include <string>
 #include <unistd.h>
 #include <vector>
+
+//TODO: change this path when merge the project
+//#include "../../include/MapPoint.h"
+
 using namespace std;
-
-
 namespace WMH
 {
 
 typedef struct Point3f{
-    float x;
-    float y;
-    float z;
+    float x = 0.0;
+    float y = 0.0;
+    float z = 0.0;
 }Point3f;
 
 enum NodePos{
@@ -38,29 +40,42 @@ enum NeighborOrientation{
 };
 
 typedef struct QTContent{
-    int MapPtN;
+    unsigned long MapPtN = 0;
     std::vector<Point3f*> MapPts;
 }QTContent;
 
 class QuadTree;
 class QTNode{
 public:
-    QTNode(int depth, float size, Point3f* center, QTNode* parent, QTContent* content);
-    bool isPointIn(Point3f* Pt);
-    vector<QTNode*> QueryNeighborNode(NeighborOrientation Orientation);
+    QTNode(int depth, float size, Point3f center, QTNode* parent);
+    void setNodeId(int id);
+    int  getNodeId();
+
+    void setContent(const QTContent &mContent);
+    const QTContent &getContent() const;
+
+    void setCenter(const Point3f &mCenter);
+    const Point3f &getCenter() const;
+
+
+    bool isPointIn(Point3f Pt);
+    void QueryNeighborNode(NeighborOrientation Orientation, QTNode* Node);
     int InitChildren(QTContent* content);
+    void PrintNodeInfo();
+
 
 
 private:
     float mMinX,mMaxX,mMinY,mMaxY,mMinZ,mMaxZ;
     int mNodeId;  // For each layer DL:00 -> 0x0 DR:01->0x1 UL:10->0x2 UR:11->0x3
-    Point3f* mCenter;
+    Point3f mCenter;
     float mSize;
-    int mDepth,mMaxDepth;
+    int mDepth,mMaxDepth;  // RootNode with MaxDepth, depth -1 for child
     QuadTree* mTree;
     QTNode* mParent;
-    QTNode* mChildren[4]; // 00 -> 0x0 01->0x1 10->0x2 11->0x3
-    QTContent* mContent;
+//    QTNode  mChildren[4]; // 00 -> 0x0 01->0x1 10->0x2 11->0x3
+    vector<QTNode> mChildren;
+    QTContent mContent;
     QTNode* mLeftNb;
     QTNode* mRightNb;
     QTNode* mUpperNb;
@@ -73,27 +88,29 @@ private:
 //  z                       z
 //  ^                       ^
 //  +----+----+             +----+----+
-//  | 10 | 11 |             | 2  | 3  |
+//  | 10 | 11 |             |2:UL|3:UR|
 //  +----+----+             +----+----+
-//  | 00 | 01 |             | 0  | 1  |
-//  +----+----|  --> x      +----+----|  --> x
+//  | 00 | 01 |             |0:DL|1:DR|
+//  +----+----+  --> x      +----+----+  --> x
 // x+ : right; y+ : down; z+ : front;
 // 00 -> 0x0 01->0x1 10->0x2 11->0x3
 
 class QuadTree {
 public:
-   QTNode* RootNode;
 
 
 public:
     QuadTree();
-    QTNode* SearchNeighbor();
+    QuadTree(int MaxDepth,vector<Point3f*> MapPoints);
+
     QTNode* QueryNodeById(int NodeId);
     QTNode* QueryPt();
 
     void SayHello(const string &someting);
+    QTContent FillContentWithMapPoints(vector<Point3f*> MapPoints);
 
 private:
+    QTNode* RootNode;
     string mpWord;
 
 }; // class QuadTree
